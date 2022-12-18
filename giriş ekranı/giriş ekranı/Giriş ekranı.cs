bb88454ecbf0750ac683c77a701f6c4b4cb10496
arda tekniker <a.ardatekniker@hotmail.com>
2022-12-19 00:13:05 +0300

    sa

---
+++ giriş ekranı/giriş ekranı/Giriş ekranı.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace giriş_ekranı
+{
+    public partial class Giriş_ekranı : Form
+    {
+        public Giriş_ekranı()
+        {
+            InitializeComponent();
+        }
+    }
+}
