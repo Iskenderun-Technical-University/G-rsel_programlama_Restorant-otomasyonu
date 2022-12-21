060c1627369d395657343da86e6cd867f0eac039
arda tekniker <a.ardatekniker@hotmail.com>
2022-12-21 19:05:52 +0300

    Menü düzenleme

--- Menü/Menü/menü.Designer.cs
+++ Menü/Menü/menü.Designer.cs
@@ -30,17 +30,22 @@
         {
             this.button8 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(59, 316);
+            this.button8.Location = new System.Drawing.Point(21, 327);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(88, 65);
             this.button8.TabIndex = 7;
@@ -49,67 +54,83 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(176, 316);
+            this.button1.Location = new System.Drawing.Point(21, 185);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 65);
             this.button1.TabIndex = 8;
             this.button1.Text = "Randevu";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(176, 220);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(88, 65);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Z Raporu";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(59, 220);
+            this.button3.Location = new System.Drawing.Point(21, 43);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(88, 65);
             this.button3.TabIndex = 10;
             this.button3.Text = "Masa Ekleme";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(176, 127);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(88, 65);
-            this.button4.TabIndex = 11;
-            this.button4.Text = "Masa";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(59, 127);
+            this.button5.Location = new System.Drawing.Point(21, 114);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(88, 65);
             this.button5.TabIndex = 12;
             this.button5.Text = "Ürün Ekleme";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // dataGridView1
             // 
-            this.button6.Location = new System.Drawing.Point(176, 34);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(88, 65);
-            this.button6.TabIndex = 13;
-            this.button6.Text = "İçecek";
-            this.button6.UseVisualStyleBackColor = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(323, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(194, 347);
+            this.dataGridView1.TabIndex = 15;
             // 
-            // button7
+            // dataGridView2
             // 
-            this.button7.Location = new System.Drawing.Point(59, 34);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(88, 65);
-            this.button7.TabIndex = 14;
-            this.button7.Text = "Yiyecek";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(555, 34);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(189, 347);
+            this.dataGridView2.TabIndex = 16;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(21, 256);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 65);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Hesap Al";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(152, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(152, 159);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 20;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(115, 205);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 21;
             // 
             // menü
             // 
@@ -117,30 +138,39 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Red;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button8);
             this.Name = "menü";
             this.ShowIcon = false;
             this.Text = "İSTE OTOMASYON";
             this.Load += new System.EventHandler(this.menü_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
